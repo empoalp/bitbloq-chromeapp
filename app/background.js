9a1c6831737a0027b5401371414f43bf52c3569d
Alberto Valero <alberto.valero@bq.com>
2019-02-22 14:38:20 +0100

    udpated to support >v 72 Chrome version

--- app/background.js
+++ app/background.js
@@ -7,121 +7,118 @@ var connectedPort;
 
 // when the webpage sends a message and we receive it, pass on the info to the uploader and request a flash to the device.
 chrome.runtime.onConnectExternal.addListener(function(port) {
-    connectedPort = port;
-    port.postMessage('connected');
-    port.onMessage.addListener(function(msg) {
-        console.log('msg', msg);
-        switch (msg.type) {
-            case 'upload':
-                console.log(msg.file);
-                //resetBoard(port, function() {
-                upload(msg, port);
-                //});
+  connectedPort = port;
+  port.postMessage('connected');
+  port.onMessage.addListener(function(msg) {
+    console.log('msg', msg);
+    switch (msg.type) {
+      case 'upload':
+        console.log(msg.file);
+        //resetBoard(port, function() {
+        upload(msg, port);
+        //});
 
-                break;
-            case 'ping':
-                port.postMessage('ping');
-                break;
-            case 'serial-connect':
-                connectSerial(msg);
-                break;
-            case 'serial-connect-send':
-                receiveData(msg.data);
-                break;
-            case 'serial-disconnect':
-                disconnectSerial();
-                break;
-            case 'change-baudrate':
-                changeBaudarate(msg.data);
-                break;
-            case 'get-ports':
-                getPorts(port);
-                break;
-            default:
-                console.log('undefined type');
-        }
-
-    });
+        break;
+      case 'ping':
+        port.postMessage('ping');
+        break;
+      case 'serial-connect':
+        connectSerial(msg);
+        break;
+      case 'serial-connect-send':
+        receiveData(msg.data);
+        break;
+      case 'serial-disconnect':
+        disconnectSerial();
+        break;
+      case 'change-baudrate':
+        changeBaudarate(msg.data);
+        break;
+      case 'get-ports':
+        getPorts(port);
+        break;
+      default:
+        console.log('undefined type');
+    }
+  });
 });
 var MBOT_LOAD_ADDRESS_REGEXP = /55....20/; //mBot always give timeout with this codes
 function upload(msg, port) {
-    // call flash process
-    serialPortConnection.disconnect(function() {
-        uploader.flash(msg.board, msg.file, function(error) {
-            var message = {
-                type: 'upload'
-            };
-            console.log(error);
-            if (!error ||
-                (error &&
-                    (msg.board === 'uno') &&
-                    error.message &&
-                    (error.message.indexOf('timeout') !== -1) &&
-                    MBOT_LOAD_ADDRESS_REGEXP.test(error.message)
-                )
-            ) {
-                message.success = true;
+  // call flash process
+  serialPortConnection.disconnect(function() {
+    uploader.flash(msg.board, msg.file, function(error) {
+      var message = {
+        type: 'upload',
+      };
+      console.log(error);
+      if (
+        !error ||
+        (error &&
+          msg.board === 'uno' &&
+          error.message &&
+          error.message.indexOf('timeout') !== -1 &&
+          MBOT_LOAD_ADDRESS_REGEXP.test(error.message))
+      ) {
+        message.success = true;
+      } else {
+        message.success = false;
+        message.error = error.message;
+      }
 
-            } else {
-                message.success = false;
-                message.error = error.message;
-            }
-
-            // send back the status of the flash to the webpage so it knows when it's done/errored.
-            port.postMessage(message);
-            serialPortConnection.reconnect();
-        });
+      // send back the status of the flash to the webpage so it knows when it's done/errored.
+      port.postMessage(message);
+      serialPortConnection.reconnect();
     });
+  });
 }
 
 function disconnectSerial() {
-    serialPortConnection.disconnect(function() {
-        console.log('reload');
-        chrome.runtime.reload();
-    });
-
+  serialPortConnection.disconnect(function() {
+    console.log('reload');
+    chrome.runtime.reload();
+  });
 }
 
 function connectSerial(msg) {
-    console.log('connectSerial', msg.port);
-    serialPortConnection.connect({
-        path: msg.port.comName,
-        endLineCharacter: '\n'
-    });
+  console.log('connectSerial', msg.port);
+  serialPortConnection.connect({
+    path: msg.port.comName,
+    endLineCharacter: '\n',
+  });
 }
 
 function receiveData(data) {
-    serialPortConnection.send(data, function(response) {
-        console.log('response');
-        console.log(response);
-    });
+  serialPortConnection.send(data, function(response) {
+    console.log('response');
+    console.log(response);
+  });
 }
 
 function changeBaudarate(baudrate) {
-    serialPortConnection.update(baudrate, function(result) {
-        console.log('hecho');
-        console.log(result);
-    });
+  serialPortConnection.update(baudrate, function(result) {
+    console.log('hecho');
+    console.log(result);
+  });
 }
 
 function getPorts() {
-    portDetection.getPorts(function(err, ports) {
-        console.log('ports');
-        console.log(ports);
-        var message = {
-            type: 'get-ports',
-            ports: ports
-        };
-        if (err) {
-            message.success = false;
-            message.error = err.message;
-        } else {
-            message.success = true;
-        }
+  portDetection.getPorts(function(err, ports) {
+    console.log('ports');
+    console.log(ports);
+    var message = {
+      type: 'get-ports',
+      ports: ports,
+    };
+    if (err) {
+      message.success = false;
+      message.error = err.message;
+    } else {
+      message.success = true;
+    }
 
-        // send back the status of the flash to the webpage so it knows when it's done/errored.
-        connectedPort.postMessage(message);
-    });
+    // send back the status of the flash to the webpage so it knows when it's done/errored.
+    connectedPort.postMessage(message);
+  });
 }
 
 /*function resetBoard(path, callback) {
@@ -180,21 +177,21 @@ function getPorts() {
 
 var serialPortConnection = new serialLib.SerialConnection();
 
-serialPortConnection.onConnect.addListener(function() {
-    console.log('connected');
-});
+serialPortConnection.onConnect = function() {
+  console.log('connected');
+};
 
 var canSendData = true;
 var buffer = '';
 
-serialPortConnection.onReadLine.addListener(function(line) {
-    buffer = buffer + line;
-    if (canSendData) {
-        connectedPort.postMessage(buffer);
-        buffer = '';
-        canSendData = false;
-        setTimeout(function() {
-            canSendData = true;
-        }, 100);
-    }
-});
+serialPortConnection.onReadLine = function(line) {
+  buffer = buffer + line;
+  if (canSendData) {
+    connectedPort.postMessage(buffer);
+    buffer = '';
+    canSendData = false;
+    setTimeout(function() {
+      canSendData = true;
+    }, 100);
+  }
+};
--- app/lib/serial.js
+++ app/lib/serial.js
@@ -4,113 +4,135 @@ const serial = chrome.serial;
 var utils = require('./utils');
 
 exports.SerialConnection = function() {
-    this.connectionId = -1;
-    this.lineBuffer = '';
-    this.lastConnectionParams = '';
-    this.endLineCharacter = false;
-    this.onConnect = new chrome.Event();
-    this.onReadLine = new chrome.Event();
-    this.onError = new chrome.Event();
-    this.sendData = false;
-    chrome.serial.onReceive.addListener(this.onReceive.bind(this));
-    chrome.serial.onReceiveError.addListener(this.onReceiveError.bind(this));
+  this.connectionId = -1;
+  this.lineBuffer = '';
+  this.lastConnectionParams = '';
+  this.endLineCharacter = false;
+  this.sendData = false;
+  chrome.serial.onReceive.addListener(this.onReceive.bind(this));
+  chrome.serial.onReceiveError.addListener(this.onReceiveError.bind(this));
 };
 
+exports.SerialConnection.prototype.onConnectComplete = function(
+  connectionInfo
+) {
+  if (!connectionInfo) {
+    console.log('Connection failed.');
+    return;
+  }
 
-exports.SerialConnection.prototype.onConnectComplete = function(connectionInfo) {
-    if (!connectionInfo) {
-        console.log('Connection failed.');
-        return;
-    }
-
-    console.log('connectionInfo');
-    console.log(connectionInfo);
-    this.connectionId = connectionInfo.connectionId;
-    this.lineBuffer = '';
-    this.onConnect.dispatch();
-    setTimeout((function() {
-        this.sendData = true;
-    }).bind(this), 500);
-
+  console.log('connectionInfo');
+  console.log(connectionInfo);
+  this.connectionId = connectionInfo.connectionId;
+  this.lineBuffer = '';
+  if (this.onConnect) {
+    this.onConnect();
+  }
+  setTimeout(
+    function() {
+      this.sendData = true;
+    }.bind(this),
+    500
+  );
 };
 
 exports.SerialConnection.prototype.onReceive = function(receiveInfo) {
-    if (receiveInfo.connectionId !== this.connectionId) {
-        //console.log('no será esto2', receiveInfo, utils.ab2str(receiveInfo.data));
-    } else {
-        //console.log('received');
-        //console.log(receiveInfo.connectionId, this.connectionId);
-        //console.log('this.endLineCharacter', this.endLineCharacter);
+  if (receiveInfo.connectionId !== this.connectionId) {
+    //console.log('no será esto2', receiveInfo, utils.ab2str(receiveInfo.data));
+  } else {
+    //console.log('received');
+    //console.log(receiveInfo.connectionId, this.connectionId);
+    //console.log('this.endLineCharacter', this.endLineCharacter);
 
-        this.lineBuffer += utils.ab2str(receiveInfo.data);
+    this.lineBuffer += utils.ab2str(receiveInfo.data);
 
-        if (this.lineBuffer.indexOf(this.endLineCharacter) !== -1 || this.lineBuffer.length > 40) {
-            //console.log('lineFoound sending');
-            //console.log(this.lineBuffer);
-            //console.log('lineFoound endsending');
-            if (this.sendData) {
-                this.onReadLine.dispatch(this.lineBuffer);
-            }
-            this.lineBuffer = '';
+    if (
+      this.lineBuffer.indexOf(this.endLineCharacter) !== -1 ||
+      this.lineBuffer.length > 40
+    ) {
+      //console.log('lineFoound sending');
+      //console.log(this.lineBuffer);
+      //console.log('lineFoound endsending');
+      if (this.sendData) {
+        if (this.onReadLine) {
+          this.onReadLine(this.lineBuffer);
         }
+      }
+      this.lineBuffer = '';
     }
+  }
 };
 
 exports.SerialConnection.prototype.onReceiveError = function(errorInfo) {
-    console.log('onReceiveError', errorInfo);
-    this.onError.dispatch(errorInfo.error);
+  console.log('onReceiveError', errorInfo);
+  if (this.onError) {
+    this.onError(errorInfo.error);
+  }
 };
 exports.SerialConnection.prototype.reconnect = function() {
-    if (this.connectionId !== -1) {
-        console.log('we are connected, we cant reconnect');
-    } else if (!this.lastConnectionParams) {
-        console.log('we cant reconnect to serial because we never connect');
-    } else {
-        this.connect(this.lastConnectionParams);
-    }
+  if (this.connectionId !== -1) {
+    console.log('we are connected, we cant reconnect');
+  } else if (!this.lastConnectionParams) {
+    console.log('we cant reconnect to serial because we never connect');
+  } else {
+    this.connect(this.lastConnectionParams);
+  }
 };
 exports.SerialConnection.prototype.connect = function(params) {
-    if (this.connectionId !== -1) {
-        console.log('its connected, so disconection!');
-        this.disconnect((function() {
-            this.connect(params);
-        }).bind(this));
-    } else {
-        this.lineBuffer = '';
-        this.lastConnectionParams = params;
-        this.endLineCharacter = params.endLineCharacter;
-        serial.connect(params.path, {
-            bufferSize: 12288,
-            ctsFlowControl: false
-        }, this.onConnectComplete.bind(this));
-    }
+  if (this.connectionId !== -1) {
+    console.log('its connected, so disconection!');
+    this.disconnect(
+      function() {
+        this.connect(params);
+      }.bind(this)
+    );
+  } else {
+    this.lineBuffer = '';
+    this.lastConnectionParams = params;
+    this.endLineCharacter = params.endLineCharacter;
+    serial.connect(
+      params.path,
+      {
+        bufferSize: 12288,
+        ctsFlowControl: false,
+      },
+      this.onConnectComplete.bind(this)
+    );
+  }
 };
 
 exports.SerialConnection.prototype.update = function(baudrate, callback) {
-    serial.update(this.connectionId, {
-        bitrate: baudrate
-    }, callback);
+  serial.update(
+    this.connectionId,
+    {
+      bitrate: baudrate,
+    },
+    callback
+  );
 };
 
 exports.SerialConnection.prototype.send = function(msg, callback) {
-    if (this.connectionId < 0) {
-        throw 'Invalid connection';
-    }
-    var msg2 = utils.str2ab(msg);
-    console.log('/********************/');
-    console.log(utils.ab2str(msg2));
-    console.log('/********************/');
-    serial.send(this.connectionId, msg2, callback);
+  if (this.connectionId < 0) {
+    throw 'Invalid connection';
+  }
+  var msg2 = utils.str2ab(msg);
+  console.log('/********************/');
+  console.log(utils.ab2str(msg2));
+  console.log('/********************/');
+  serial.send(this.connectionId, msg2, callback);
 };
 
 exports.SerialConnection.prototype.disconnect = function(callback) {
-    if (this.connectionId < 0) {
+  if (this.connectionId < 0) {
+    callback();
+  } else {
+    serial.disconnect(
+      this.connectionId,
+      function() {
+        this.connectionId = -1;
+        this.sendData = false;
         callback();
-    } else {
-        serial.disconnect(this.connectionId, (function() {
-            this.connectionId = -1;
-            this.sendData = false;
-            callback();
-        }).bind(this));
-    }
+      }.bind(this)
+    );
+  }
 };
